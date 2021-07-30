b3f44adaaa8f65fb7ed36a3d23a26128995883f2
balasundarambui <56162438+balasundarambui@users.noreply.github.com>
2021-07-30 16:12:43 +0530

    Notification hide/show issues

--- themes/incsol/template.php
+++ themes/incsol/template.php
@@ -258,7 +258,7 @@
                             </ul>
                             <ul class="nav-right">
                                 <li class="header-notification">
-                                    <a href="#!" class="waves-effect waves-light">
+                                    <a href="#!" class="waves-effect waves-light header-notify">
                                         <i class="ti-bell"></i>
                                         <span class="badge bg-c-red"></span>
                                     </a>
@@ -1455,6 +1455,9 @@
         <script>
             $('.excel_btn').live('click', function () {
                 fnExcelReport();
+            });
+            $('.header-notify').click(function() {
+                $('#sidebar').css({display:"none"})
             });
         </script>
         <script>
